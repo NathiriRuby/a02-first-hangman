1aa0af361777ad73ba2e27659bc4c66149208d94
NathiriRuby <cventura@smu.edu>
2018-10-05 23:25:24 -0500

    partial update to functionality

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -5,39 +5,39 @@ defmodule Hangman do
   """
 
   #the defined struct
-  defstruct game_state: "", turns_left: "7", letters: "", used: [], last_guess: "", word: ""
+  defstruct game_state: :initializing, turns_left: 7, letters: [], used: [], last_guess: "", word: ""
 
 
   def word_to_blank([_ | []]) do
-    ['_']                                #reached the end of the string, so return
+    ["_"]                                #reached the end of the string, so return
   end
  
   def word_to_blank([_ | tail]) do
-    ['_' | word_to_blank(tail)]   # turn a string into underscores
+    ["_" | word_to_blank(tail)]   # turn a string into underscores
   end
 
   #replace underscores with previous guesses. For the last character in the string
   def word_to_blank([guess_head | []], [actual_head | []], guess) do
-    if ( guess_head == "#{actual_head}") do
-      ["#{guess_head}"]
+    if ( guess_head == actual_head) do
+      [guess_head]
     else 
       if (actual_head == guess) do
-        ["#{guess}"]
+        [guess]
       else
-        ['_']
+        ["_"]
       end
     end
   end
 
   #Replace underscores with correct guesses. Remember to keep previous guesses
   def word_to_blank([guess_head | guess_tail], [actual_head | actual_tail], guess) do
-    if ( guess_head == "#{actual_head}") do 
-      ["#{guess_head}" | word_to_blank(guess_tail, actual_tail, guess)]
+    if ( guess_head == actual_head) do 
+      [guess_head | word_to_blank(guess_tail, actual_tail, guess)]
     else 
       if (actual_head == guess) do
-        ["#{guess}" | word_to_blank(guess_tail, actual_tail, guess)]
+        [guess | word_to_blank(guess_tail, actual_tail, guess)]
       else
-        ['_' | word_to_blank(guess_tail, actual_tail, guess)]
+        ["_" | word_to_blank(guess_tail, actual_tail, guess)]
       end
     end
   end
@@ -48,15 +48,19 @@ defmodule Hangman do
     IO.puts(word)                                       #debugging: show word
     blank_word = word_to_blank(word)                       #turn the letters in a word into blanks
     
-    %Hangman{game_state: :initializing, turns_left: 7, letters: blank_word, used: [], last_guess: "", word: word}
+    %Hangman{letters: blank_word, word: word}
   end
 
+  def tally(game = %{game_state: :won}) do
+      %{ game_state: game.game_state, turns_left: game.turns_left, letters: game.letters, used: game.used, last_guess: game.last_guess}
+  end
+
+  def tally(game = %{game_state: :lose}) do
+      %{ game_state: game.game_state, turns_left: game.turns_left, letters: game.letters, used: game.used, last_guess: game.last_guess}
+  end
+  
   def tally(game) do
-   if(game.game_state == :won || game.game_state == :lose) do     #replace 'in progress' word with full word if game is over
-      %{ game_state: "#{game.game_state}", turns_left: "#{game.turns_left}", letters: "#{game.word}", used: "[#{game.used}]", last_guess: "#{game.last_guess}"}
-   else
-      %{ game_state: "#{game.game_state}", turns_left: "#{game.turns_left}", letters: "[#{game.letters}]", used: "[#{game.used}]", last_guess: "#{game.last_guess}"}
-   end
+      %{ game_state: game.game_state, turns_left: game.turns_left, letters: game.letters, used: game.used, last_guess: game.last_guess}
   end
 
   # def auto_win(game) do           #debug
@@ -67,33 +71,81 @@ defmodule Hangman do
   #    %{game | game_state: :lose}
   # end
 
-  def make_move(game, guess) do 
-    if (guess in game.used) do                                     #if user already guessed that character, tell them so
-        IO.puts("Already guessed that letter!")
-    else                                                              #user guessed a new character that is in the word
-      new_used = %{game | used: Enum.sort(game.used ++ ["#{guess}"])}     #Add the new guess to the used list
-      new_guess = %{new_used | last_guess: "#{guess}"}
-      newgame = new_guess
-      if( guess in newgame.word ) do                                      #replace underscores with correct guess
-          new_word = word_to_blank(newgame.letters, newgame.word, guess)  #Add the guess characters to the 'in progress' word
-          new_letters = %{new_guess | letters: new_word}                   #update the struct with the new word
-          if(new_letters.letters == new_letters.word) do                  #If the 'in progress' word has been completed, the game is won
-            game = %{new_letters | game_state: :won}
-            {game, tally(game)}
-          else                                                            #Otherwise, just tell the user it was a good guess
-            game = %{new_letters | game_state: :good_guess}
-            {game, tally(game)}
-          end
-      else                                                                 #bad answer! -1 turn
-        newgame = %{newgame | turns_left: game.turns_left-1}              
-        if (newgame.turns_left == 0) do                                    #If the user is out of chances, end the game
-          game = %{newgame | game_state: :lose}
-          {game, tally(game)}
-        else
-          game = %{newgame | game_state: :bad_guess}
-          {game, tally(game)}
-        end
-      end
+  def make_move(game = %{game_state: :won}, _) do
+    {game, tally(game)}
+  end
+
+  def make_move(game = %{game_state: :lose}, _) do
+    {game, tally(game)}
+  end
+
+  def make_move(game, guess) do
+    game |> already_used?(guess) |> process_if_not(guess)
+    {game, tally(game)}
+  end
+
+  def already_used?(game, guess) do
+    if (guess in game.used) do
+      %{game | game_state: :already_used}
+    else
+      game
     end
   end
+
+  def process_if_not(game = %{game_state: :already_used}, _) do
+    game
+  end
+
+  def process_if_not(game, guess) do
+    record_guess(game, guess) |> good_guess?(guess) |> decide_turns_left(guess) |> decide_state |> return_tally()
+  end
+
+  def record_guess(game, guess) do
+    game = %{game | used: Enum.sort(game.used ++ [guess])}
+  end
+
+  def good_guess?(game) do
+    if (guess in game.word) do
+      game = %{game | game_state: :good_guess}
+    else
+      game = %{game | game_state: :bad_guess}
+    end
+  end
+
+  def decide_turns_left(game = %{game_state}) do
+  end
+
+  def decide_turns_left() do
+  end
+
+  # def make_move(game, guess) do 
+  #   if (guess in game.used) do                                     #if user already guessed that character, tell them so
+  #       IO.puts("Already guessed that letter!")
+  #       {game, tally(game)}
+  #   else                                                              #user guessed a new character that is in the word
+  #     new_used = %{game | used: Enum.sort(game.used ++ ["#{guess}"])}     #Add the new guess to the used list
+  #     new_guess = %{new_used | last_guess: "#{guess}"}
+  #     newgame = new_guess
+  #     if( guess in newgame.word ) do                                      #replace underscores with correct guess
+  #         new_word = word_to_blank(newgame.letters, newgame.word, guess)  #Add the guess characters to the 'in progress' word
+  #         new_letters = %{new_guess | letters: new_word}                   #update the struct with the new word
+  #         if(new_letters.letters == new_letters.word) do                  #If the 'in progress' word has been completed, the game is won
+  #           game = %{new_letters | game_state: :won}
+  #           {game, tally(game)}
+  #         else                                                            #Otherwise, just tell the user it was a good guess
+  #           game = %{new_letters | game_state: :good_guess}
+  #           {game, tally(game)}
+  #         end
+  #     else                                                                 #bad answer! -1 turn
+  #       newgame = %{newgame | turns_left: game.turns_left-1}              
+  #       if (newgame.turns_left == 0) do                                    #If the user is out of chances, end the game
+  #         game = %{newgame | game_state: :lose}
+  #         {game, tally(game)}
+  #       else
+  #         game = %{newgame | game_state: :bad_guess}
+  #         {game, tally(game)}
+  #       end
+  #     end
+  #   end
+  # end
 end
